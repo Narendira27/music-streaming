c023add3447d9ae446787f0f5eb7e0098f7bf393
Narendira <narendira2701@gmail.com>
2025-02-14 21:02:17 +0530

    fix issue

--- backend/src/utils/uploadZipToS3.ts
+++ backend/src/utils/uploadZipToS3.ts
@@ -16,6 +16,7 @@ if (!keyId || !applicationKey || !bucketName) {
 }
 
 const s3Client = new S3Client({
+  region: "us-east-005",
   endpoint: "https://s3.us-east-005.backblazeb2.com",
   credentials: {
     accessKeyId: keyId,
